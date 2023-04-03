8cbe46c7c4acff12d52b4b5aa99266cf0445fd0e
xiongcan941 <690875292@qq.com>
2023-04-03 19:31:12 +0800

    实现内核print

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
-void main() {
-  while (1) {}
+#include "monitor.h"
+char* welcome = " welcome to scroll kernel\n";
+
+static void print_welcome() {
+  monitor_print_with_color("#", COLOR_GREEN);
+  monitor_printf(welcome);
+}
+
+int main() {
+  print_welcome();
 }
---
+++ math.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_MATH_H
+#define UTILS_MATH_H
+
+#include "common.h"
+
+uint32 max(uint32 x, uint32 y);
+
+uint32 min(uint32 x, uint32 y);
+
+int32 div(int32 x, int32 N);
+
+int32 mod(int32 x, int32 N);
+
+#endif
+
